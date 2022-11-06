4c12bf39455c6ec367a55ef6077fc3c4771d5d44
Andrew Jin <111733911+blue0316@users.noreply.github.com>
2022-11-07 07:05:15 +0900

    add for cors policy

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,6 +4,8 @@ const instance = axios.create({
     headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin":
+            "https://answersheet-au-frontend-nextjs.vercel.app",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
     },
     baseURL: "https://answersheet-au-strapi-backend.herokuapp.com",
@@ -12,6 +14,8 @@ export const linstance = axios.create({
     headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin":
+            "https://answersheet-au-frontend-nextjs.vercel.app",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
     },
     baseURL: process.env.NEXT_PUBLIC_URL,
